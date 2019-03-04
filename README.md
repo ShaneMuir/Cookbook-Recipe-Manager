98364ab3e0ee4455a3d9c06ba94d6de865c9de0e
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-03-04 18:52:20 +0000

    Successfully connected PyMongo to our mLab db

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Cookbook
 Built with Flask, Python, PyMongo
 
+Recipes taken from this site :- https://developer.edamam.com/edamam-docs-recipe-api
+
+
--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello World"
+app.config["MONGO_DBNAME"] = "organic"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'monogodb://localhost')
 
+mongo = PyMongo(app)
+    
+@app.route('/')
+@app.route('/get_recipes')
+def home():
+    return render_template('index.html', recipe=mongo.db.recipe.find())
+    
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
