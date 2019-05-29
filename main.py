631a6ca3d079558044084afe5d69dbea5b923048
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-05-29 07:34:14 +0000

    Added alert box, tidy some styles and fixed some markup/style issues

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, math, re
-from flask import Flask, render_template, redirect, request, url_for, flash, session, jsonify, json
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 from forms import LoginForm, RegistrationForm, RecipeForm
@@ -17,18 +17,17 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def index():
-    """Route lets users see all recipes, logged_in users can use CRUD"""
+    """Route lets users see all recipes"""
     page_limit = 6 #Logic for pagination
     current_page = int(request.args.get('current_page', 1))
     total = mongo.db.recipe.count()
     pages = range(1, int(math.ceil(total / page_limit)) + 1)
     recipes = mongo.db.recipe.find().sort('_id', pymongo.ASCENDING).skip((current_page - 1)*page_limit).limit(page_limit)
-    
     return render_template('index.html', recipe=recipes, title='Home', current_page=current_page, pages=pages)
     
 @app.route('/recipe/<recipe_id>', methods=['GET','POST'])
 def recipe(recipe_id):
-    """Route for viewing a single recipe, logged_in users can use CRUD"""
+    """Route for viewing a single recipe"""
     a_recipe =  mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
     pprint(a_recipe)
     return render_template('recipe.html', recipe=a_recipe, title=a_recipe['recipe_name'])
@@ -38,8 +37,7 @@ def i_made_it(recipe_id):
     """Function to increment i-made-it counter"""
     mongo.db.recipe.find_one_and_update(
         {'_id': ObjectId(recipe_id)},
-        {'$inc': {'i-made-it': 1}}
-    )
+        {'$inc': {'i-made-it': 1}})
     return redirect(url_for('recipe', recipe_id=recipe_id))
 
 @app.route('/search')
@@ -51,24 +49,19 @@ def search():
     total = mongo.db.recipe.find({'$text': {'$search': db_query }})
     t_total = len([x for x in total])
     pages = range(1, int(math.ceil(t_total / page_limit)) + 1)
-    
     results = mongo.db.recipe.find({'$text': {'$search': db_query }}).sort('_id', pymongo.ASCENDING).skip((current_page - 1)*page_limit).limit(page_limit)
     return render_template('search.html', results=results, pages=pages, current_page=current_page, db_query=db_query)
 
 
 @app.route('/filtered_search', methods=['GET', 'POST'])
 def filtered():
-    """Logic for allowing filtering"""
+    """Logic for filtering recipes collection"""
     page_limit = 6 #Logic for pagination
     current_page = int(request.args.get('current_page', 1))
     total = mongo.db.recipe.count()
     pages = range(1, int(math.ceil(total / page_limit)) + 1)
     
     if request.method == "POST":
-        page_limit = 6 #Logic for pagination
-        current_page = int(request.args.get('current_page', 1))
-        total = mongo.db.recipe.count()
-        pages = range(1, int(math.ceil(total / page_limit)) + 1)
         for i in request.form:
             if i == "diet_labels":
                 filter_items = []
@@ -77,10 +70,9 @@ def filtered():
                 for item in items: # iterate through the list
                     for i in my_key: #grab key_name
                         filter_items.append({i: item})
-                print(filter_items) 
-
-                results = mongo.db.recipe.find({'$and': filter_items }).skip((current_page - 1)*page_limit).limit(page_limit)
-                return render_template('filter.html', title="Filtered Seach", results=results, current_page=current_page, pages=pages)
+                results = mongo.db.recipe.find({'$and': filter_items })
+                total_results =  mongo.db.recipe.find({'$and': filter_items }).count()
+                return render_template('filter.html', title="Filtered Seach", results=results, total_results=total_results)
             
             if i == "health_labels":
                 filter_items = []
@@ -89,12 +81,10 @@ def filtered():
                 for item in items: # iterate through the list
                     for i in my_key: #grab key_name
                         filter_items.append({i: item})
-                print(filter_items) 
-
                 results = mongo.db.recipe.find({'$and': filter_items })
-                return render_template('filter.html', title="Filtered Seach", results=results)
+                total_results = mongo.db.recipe.find({'$and': filter_items }).count()
+                return render_template('filter.html', title="Filtered Seach", results=results, total_results=total_results)
                 
-    
     recipes = mongo.db.recipe.find().sort('_id', pymongo.ASCENDING).skip((current_page - 1)*page_limit).limit(page_limit)
     return render_template('index.html', recipe=recipes, title='Home', current_page=current_page, pages=pages)
 
@@ -104,7 +94,8 @@ def create_recipe():
     """Create a new recipe to db collection"""
     if 'logged_in' not in session: #Check if its a logged in user
         flash('Sorry, only logged in users can create recipes. Please register')
-            
+        return redirect(url_for('index'))
+
     form = RecipeForm(request.form) #Initialise the form
     user = mongo.db.user.find_one({"name": session['username'].title()})
     
@@ -126,9 +117,7 @@ def create_recipe():
         'username' : session['username'],
         'created_by': {
             '_id': user['_id'],
-            'name': user['name']
-        }
-        })
+            'name': user['name']}})
         flash('Recipe Added!')
         return redirect(url_for('index'))
     return render_template('add_recipe.html', form=form)
@@ -142,7 +131,6 @@ def edit_recipe(recipe_id):
     
     user = mongo.db.user.find_one({"name": session['username'].title()})
     the_recipe = mongo.db.recipe.find_one_or_404({'_id': ObjectId(recipe_id)})
-    
     form = RecipeForm()
     
     if user['name'].title() == the_recipe['username'].title(): #If user created then user can edit
@@ -166,8 +154,7 @@ def edit_recipe(recipe_id):
             'cooking_time': request.form['cooking_time'],
             'description': request.form['description'],
             'source': request.form['source']
-                }
-            })
+                }})
             flash('Recipe updated.')
             return redirect(url_for('recipe', recipe_id=recipe_id))
     flash("Sorry this is not your recipe to edit!") #In case a user tried to enter URL manually
@@ -179,7 +166,7 @@ def delete_recipe(recipe_id):
     if session:
         user = mongo.db.user.find_one({"name": session['username'].title()})
         the_recipe = mongo.db.recipe.find_one_or_404({'_id': ObjectId(recipe_id)})
-        
+
         if user['name'].title() == the_recipe['username'].title(): #If user created then user can delete
             recipe = mongo.db.recipe
             recipe.delete_one({
@@ -194,12 +181,11 @@ def delete_recipe(recipe_id):
         flash('Sorry, only logged in user can view this page')
         return redirect(url_for('index'))
     
-
-        
+    
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Function for handling the registration of users"""
-    if 'logged_in' == True:#Check is user already logged in
+    if 'logged_in' in session: #Check is user already logged in
         return redirect(url_for('index'))
     
     form = RegistrationForm()
@@ -227,11 +213,11 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
     """Function for handling the logging in of users"""
+    
     if 'logged_in' in session: #Check is already logged in
         return redirect(url_for('index'))
-    
+
     form = LoginForm()
-    
     if form.validate_on_submit():
         user = mongo.db.user
         logged_in_user = user.find_one({'name' : request.form['username'].title()})
@@ -241,9 +227,8 @@ def user_login():
                 session['username'] = request.form['username']
                 session['logged_in'] = True
                 return redirect(url_for('index'))
-            flash('Sorry, incorrect password.')
+            flash('Sorry incorrect password!')
             return redirect(url_for('user_login'))
-   
     return render_template('login.html', form=form, title='Login')
     
     
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,14 @@
-// Materialize Sidenav
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-  
-  
-  
+$(document).ready(function() {
+    //Materialize function
+    $('select').formSelect();
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    //End of Materialize functions
+    
+    //My functions
+    $('#alert_close').click(function() {
+        $("#alert_box").fadeOut("slow", function() {});
+    });
+    //End of my functions
+
+});
